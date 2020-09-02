dff9bfd2795a0fcd39841bc48143a5ece3c392e0
pucho <2104724@talnet.nl>
2020-09-02 18:13:43 +0200

    updated

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+
+echo var_dump($klas);
+
+ ?>
+
+<?php
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+
+for ($i=0; $i <1 ; $i++) {
+  echo implode("\n", $klas);
+}
+
+ ?>
+
+<?php
+
+$bucket_list = array();
+$hoeveelheid_activiteiten;
+
+echo "Hoeveel activiteiten hij/zij wil toevoegen aan de bucketlist ";
+$input = rtrim(fgets(STDIN));
+
+ ?>
